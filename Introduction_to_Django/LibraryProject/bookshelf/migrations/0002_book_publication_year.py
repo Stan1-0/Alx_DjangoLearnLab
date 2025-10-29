c7b254629eb815f8a02b06193374d5ad172c170e
Stan1-0 <stankofb@gmail.com>
2025-10-29 23:22:14 +0000

    added a book

---
+++ Introduction_to_Django/LibraryProject/bookshelf/migrations/0002_book_publication_year.py
@@ -0,0 +1,19 @@
+# Generated manually
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bookshelf', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='book',
+            name='publication_year',
+            field=models.IntegerField(blank=True, null=True),
+        ),
+    ]
+
