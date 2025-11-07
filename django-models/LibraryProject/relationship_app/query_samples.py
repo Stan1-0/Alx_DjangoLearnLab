c4c126cc211618b11b1e475cbe3d6906aeb25cd6
Stan1-0 <stankofb@gmail.com>
2025-11-07 22:29:50 +0000

    edited a model

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,11 @@
 from .models import *
 
-books.all()
+
 
 Library.objects.get(name=library_name)
 
 Author.objects.get(name=author_name)
-objects.filter(author=author)
+
 
 Librarian.objects.get(library=librarian)
 
