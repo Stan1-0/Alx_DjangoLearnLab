48b300edf218e3f9c3dfb054a9d1a5f3baecdf15
Stan1-0 <stankofb@gmail.com>
2025-11-08 21:05:34 +0000

    `Added user authentication views and updated URLs for registration, login, and logout in relationship_app; included static template loading in HTML files`

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import list_books
-from .views import LibraryDetailView 
+from .views import LibraryDetailView
+from .views import Register
 
 urlpatterns = [
     path('', list_books, name='list-all-books'),
     path('<int:library_pk>/books/', LibraryDetailView.as_view(), name="library_book_list"),
-    path('<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
+    path('<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library 
 from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
 
 # Create your views here.
 def list_books(request):
@@ -26,3 +30,15 @@ class LibraryDetailView(DetailView):
         books = [Book.objects.get(pk=book.id) for book in library_books]
         context.update({'books': books})
         return context
+    
+class Register(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "relationship_app/register.html"
+
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+    redirect_authenticated_user = True
+
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
