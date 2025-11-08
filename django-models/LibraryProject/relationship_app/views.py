b4fb5f00a1263b73ce2a2eb682247ad95b805372
Stan1-0 <stankofb@gmail.com>
2025-11-08 21:11:05 +0000

    Updated views.py to include login import and resolved binary file differences in compiled files for settings, urls, and views in relationship_app.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
 from django.views.generic import CreateView
 
 # Create your views here.
