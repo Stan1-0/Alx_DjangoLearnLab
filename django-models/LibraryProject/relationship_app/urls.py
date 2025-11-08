34edb8af07b1f591ac9f09a03e6ac3c603fb8b63
Stan1-0 <stankofb@gmail.com>
2025-11-08 21:22:34 +0000

    Updated login and logout URL patterns in relationship_app to specify custom template paths for improved user experience.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('<int:library_pk>/books/', LibraryDetailView.as_view(), name="library_book_list"),
     path('<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path("register/", register.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     
 
 ]
