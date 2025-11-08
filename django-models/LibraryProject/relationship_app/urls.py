18223b28c8b8157a48ee0c28b6fbde16fd277654
Stan1-0 <stankofb@gmail.com>
2025-11-08 21:38:21 +0000

    Updated registration URL in urls.py to use the correct function import from views for consistency.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,16 @@
-from django.contrib.auth import views
+
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
 from .views import register, LoginView, LogoutView
 
 
+
 urlpatterns = [
     path('', list_books, name='list-all-books'),
     path('<int:library_pk>/books/', LibraryDetailView.as_view(), name="library_book_list"),
     path('<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path("register/", register.as_view(), name="register"),
+    path("register/", register, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library 
 from django.views.generic.detail import DetailView
@@ -32,10 +32,15 @@ class LibraryDetailView(DetailView):
         context.update({'books': books})
         return context
     
-class register(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "relationship_app/register.html"
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
 class LoginView(LoginView):
     template_name = "relationship_app/login.html"
