9b3f9389072523626738921c6af0fb3a8183ed31
Stan1-0 <stankofb@gmail.com>
2025-11-09 00:06:20 +0000

    Update UserProfile model to change role 'user' to 'member' for improved clarity in role management

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -34,7 +34,7 @@ class UserProfile(models.Model):
     ROLES = [
         ('admin', 'Admin'),
         ('librarian', 'Librarian'),
-        ('user', 'User'),
+        ('member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     role = models.CharField(max_length=20, choices=ROLES, default='user')
