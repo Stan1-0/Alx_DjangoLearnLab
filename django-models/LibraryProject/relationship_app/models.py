5593f5f36e0cb1f366a5b5ab3dc9039520a8c02e
Stan1-0 <stankofb@gmail.com>
2025-11-09 12:48:01 +0000

    Fix typo in permissions definition for Book model in relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
     class Meta:
-        permissons:(
+        permissions:(
             ("can_add_book", "Can add a book"),
             ("can_change_book", "Can change a book"),
             ("can_delete_book", "Can delete a book")
