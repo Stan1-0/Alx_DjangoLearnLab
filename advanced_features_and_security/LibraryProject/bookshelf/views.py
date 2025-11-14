e18a20e638765865821dd52e2cca410985d63295
Stan1-0 <stankofb@gmail.com>
2025-11-14 23:46:17 +0000

    Refactor book_list view to ExampleView and update template rendering
    - Renamed the book_list view to ExampleView for clarity.
    - Updated the view to render a new template, form_example.html, instead of list_books.html.
    - Imported ExampleForm for potential future use in the view.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
+from .forms import ExampleForm
 from .models import Book
 
 # Create your views here.
 @permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
+def ExampleView(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'bookshelf/form_example.html', {'books': books})
