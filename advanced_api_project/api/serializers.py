08a7d1d3bf3c6fc45db37578478ec8b2fdebe87b
Stan1-0 <stankofb@gmail.com>
2025-11-27 13:34:15 +0000

    created models and serializers

---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import *
+from datetime import date
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+        
+    def validate_publication_year(self, value):
+        today = date.today().year
+        if value > today:
+            raise serializers.ValidationError("Publication year cannot be in the future")
+        return value
+            
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Author
+        fields = ['name']
