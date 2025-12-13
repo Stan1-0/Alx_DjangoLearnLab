11cf2eca1113e1ae19ed64831553c790cfc024f9
Stan1-0 <stankofb@gmail.com>
2025-12-13 16:07:11 +0000

    created posts

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from accounts.serializers import CustomUserSerializer, RegisterSerializer, LoginSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 # Create your views here.
 class CustomUserRegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
         
 class CustomUserLoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
