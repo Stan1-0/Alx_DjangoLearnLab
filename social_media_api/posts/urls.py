4c1f66536b925cf03deff00dca5e0fec147b1cdb
Stan1-0 <stankofb@gmail.com>
2025-12-13 22:14:39 +0000

    updated readme

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/', views.PostListCreateView.as_view(), name='post-list-create'),
+    path('', views.PostListCreateView.as_view(), name='post-list-create'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('<int:post_pk>/comments/', views.CommentListCreateView.as_view(), name='comment-list-create'),
     path('<int:post_pk>/comments/<int:pk>', views.CommentDetailView.as_view(), name='comment-detail'),
