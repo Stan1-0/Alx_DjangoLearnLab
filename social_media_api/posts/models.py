33335d1af509621f9f4e03ff54b0724379e5e7e8
Stan1-0 <stankofb@gmail.com>
2025-12-13 23:54:25 +0000

    created a like model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -22,3 +22,13 @@ class Comment(models.Model):
         return f'Comment by {self.author.username} on {self.post.title}'
     
     
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f'Liked by {self.user.username} on {self.post.title}'
